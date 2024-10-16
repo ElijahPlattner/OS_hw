64f5dafe9729ad4add146395c36caa373ca5cd5a
Elijah Plattner <elijahplattner@elijahs-mbp-3.mynetworksettings.com>
2024-10-16 14:29:11 -0500

    add enqueue and dequeue

--- kernel.h
+++ kernel.h
@@ -1,15 +1,19 @@
 #include "libos.h"
 #include <stdint.h>
 
+#define NULL ((void*)0) 
+
 typedef struct {
     uint64_t sp;
     uint32_t pid;
+    PCB_t * next;
+
 } PCB_t;
 
 typedef struct {
 
-    struct Node_t *front;
-    struct Node_t *rear;
+    PCB_t * head;
+    PCB_t * tail;
 
 } PCB_Q_t;
 
@@ -24,10 +28,35 @@ void clear_scr(int srow, int scol, int erow, int ecol){
 
 void enqueue(PCB_Q_t *q, PCB_t *pcb){
 
+    //if the queue has nothing in it
+    if (q->tail == NULL){
+        q->head = q->tail = pcb;
+        pcb->next = NULL;
+        return;
+    }
+
+    //otherwise add pcb to the end of the queue
+    q->tail->next = pcb;
+    q->tail = pcb;
+    q->tail->next = NULL;
 
 };
 
 PCB_t *dequeue(PCB_Q_t *q){
+
+    //if queue is empty
+    if (q->tail == NULL){
+        return NULL;
+    } 
+
+    //store the head of queue and update head
+    PCB_t * temp = q->head;
+    q->head = q->head->next;
+
+    if (q->head == NULL)
+        q->tail = NULL;
+    
+    return temp;
 
 
 };
