ef3ea6ea1064d97be5a1ce2c635f69268f9b4d1c
Elijah Plattner <elijahplattner@Elijahs-MacBook-Pro-3.local>
2024-10-18 00:05:19 -0500

    add stack allocation

---
+++ stack_alloc.h
@@ -0,0 +1,25 @@
+#ifndef PCB_STACK_H
+#define PCB_STACK_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+//define macros
+#define MAX_STACKS 10
+#define STACK_SIZE 1024
+
+//declare the stacks
+uint64_t stacks [MAX_STACKS][STACK_SIZE];
+uint32_t next_stack = 0;
+
+
+uint64_t *alloc_stack (){
+
+    if(next_stack == MAX_STACKS)
+        return NULL; //ELIJAH add error handling
+    
+    return stacks[next_stack++];
+};
+
+#endif
+
