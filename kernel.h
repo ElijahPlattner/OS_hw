f5ccc0704af40ea97d86eb64637fc69f7b2da3f1
Elijah Plattner <elijahplattner@Elijahs-MacBook-Pro-3.local>
2024-10-18 00:19:00 -0500

    add process function to kernel

--- kernel.h
+++ kernel.h
@@ -1,22 +1,11 @@
 #include "libos.h"
 #include <stdint.h>
+#include "pcb_alloc.h"
+#include "stack_alloc.h"
 
-#define NULL ((void*)0) 
-
-typedef struct {
-    uint64_t sp;
-    uint32_t pid;
-    PCB_t * next;
-
-} PCB_t;
-
-typedef struct {
-
-    PCB_t * head;
-    PCB_t * tail;
-
-} PCB_Q_t;
-
+//global variable
+int next_pid = 0;
+PCB_Q_t* q;
 
 void clear_scr(int srow, int scol, int erow, int ecol){
 
@@ -61,3 +50,45 @@ PCB_t *dequeue(PCB_Q_t *q){
 
 };
 
+int create_process(int (*code_address)()) {
+
+    //set stackptr to return value of calling alloc_stack() to allocate a stack
+    uint64_t stackptr = alloc_stack();
+
+    //check to make sure stackptr is not null 
+    if(stackptr == NULL){
+        return -1;
+    } 
+    
+    //set stack pointer (sp) to the bottom of the stack
+    uint32_t * sp = stackptr + 1024;
+    
+    //create context that will set gen purpose regs to 0
+    for(int i =1; i <= 31; i++){
+        sp--;
+
+        //set the value at which sp+30 points to code_address
+        if(i==30)
+            *sp = (uint64_t)code_address;
+        else
+            *sp = 0;
+    }
+
+    //     set pcb to the return value of alloc_pcb() to allocate a pcb for the process
+    PCB_t * pcb= alloc_pcb();
+
+	//check the pcb and if is null then return -1
+    if(pcb == NULL)
+        return -1;
+
+    //set the pcb's sp member to sp.
+    //set the pcb's pid member to next_pid
+    pcb->sp = sp;
+    pcb->pid= next_pid++;
+
+    //enqueue the pcb onto the ready queue  // ready queue will be a global
+    enqueue(q, pcb);
+
+    return 0;
+
+    }
