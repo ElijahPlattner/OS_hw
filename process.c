553d0cb8e17db6c3a51339467d68a7f109269550
ElijahPlattner <ecplatt@outlook.com>
2024-10-22 11:29:12 -0500

    split process.h & c

--- process.c
+++ process.c
@@ -1,48 +1,44 @@
-#include <stdint.h>
-#include "pcb_alloc.h"
-#include "stack_alloc.h"
-#include "globals.h"
-
+#include "process.h"
 
 int create_process(int (*code_address)()) {
-
-    //set stackptr to return value of calling alloc_stack() to allocate a stack
+    // Set stackptr to return value of calling alloc_stack() to allocate a stack
     uint64_t stackptr = alloc_stack();
 
-    //check to make sure stackptr is not null 
-    if(stackptr == NULL){
+    // Check to make sure stackptr is not null 
+    if (stackptr == 0) {
         return -1;
-    } 
-    
-    //set stack pointer (sp) to the bottom of the stack
-    uint32_t * sp = stackptr + 1024;
-    
-    //create context that will set gen purpose regs to 0
-    for(int i =1; i <= 31; i++){
-        sp--;
-
-        //set the value at which sp+30 points to code_address
-        if(i==30)
-            *sp = (uint64_t)code_address;
-        else
-            *sp = 0;
     }
 
-    //     set pcb to the return value of alloc_pcb() to allocate a pcb for the process
-    PCB_t * pcb= alloc_pcb();
+    // Set stack pointer (sp) to the bottom of the stack
+    uint32_t *sp = (uint32_t *)(stackptr + 1024);
 
-	//check the pcb and if is null then return -1
-    if(pcb == NULL)
+    // Create context that will set general purpose registers to 0
+    for (int i = 1; i <= 31; i++) {
+        sp--;
+
+        // Set the value at which sp+30 points to code_address
+        if (i == 30) {
+            *sp = (uint64_t)code_address;
+        } else {
+            *sp = 0;
+        }
+    }
+
+    // Set PCB to the return value of alloc_pcb() to allocate a PCB for the process
+    PCB_t *pcb = alloc_pcb();
+
+    // Check if PCB is null
+    if (pcb == NULL) {
         return -1;
+    }
 
-    //set the pcb's sp member to sp.
-    //set the pcb's pid member to next_pid
+    // Set the PCB's sp member to sp
+    // Set the PCB's pid member to next_pid
     pcb->sp = sp;
-    pcb->pid= next_pid++;
+    pcb->pid = next_pid++;
 
-    //enqueue the pcb onto the ready queue  // ready queue will be a global
+    // Enqueue the PCB onto the ready queue (q is a global)
     enqueue(q, pcb);
 
     return 0;
-
-    };
+}
---
+++ process.h
@@ -0,0 +1,12 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <stdint.h>
+#include "pcb_alloc.h"
+#include "stack_alloc.h"
+#include "globals.h"
+
+// Function declaration
+int create_process(int (*code_address)());
+
+#endif // PROCESS_H
