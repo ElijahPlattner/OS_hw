1317752a9f35af9f2ae5b0da20dfcbac775834de
Elijah Plattner <elijahplattner@MacBookPro.tntech.edu>
2024-10-21 19:15:04 -0500

    add globals header

---
+++ globals.h
@@ -0,0 +1,14 @@
+//pcb structs
+#include "pcb.h"
+
+//include guard
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+
+//global variables
+extern int next_pid = 0;
+extern PCB_Q_t* q;
+extern PCB_t* Running;
+
+#endif
